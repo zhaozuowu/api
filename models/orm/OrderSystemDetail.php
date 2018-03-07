af2fe6a49948c3ece2f01780885538d01e1f2fee
jinyu02 <jinyu02@iwaimai.baidu.com>
2018-03-07 22:04:06 +0800

    omsui orm
    Change-Id: I8c9a4c3512989476ea72f63619cef8722a1016de

--- models/orm/OrderSystemDetail.php
+++ models/orm/OrderSystemDetail.php
@@ -2,21 +2,22 @@
 
 /**
  * @property int $id
+ * @property int $order_system_id
+ * @property int $order_type
  * @property int $business_form_order_id
- * @property int $sku_id
- * @property int $sku_amount
- * @property string $sku_ext
- * @property string $sku_exception
+ * @property int $parent_order_id
+ * @property int $order_id
+ * @property string $order_exception
  * @property int $create_time
  * @property int $update_time
  * @property int $is_delete
  * @property int $version
- * @method static Model_Orm_BusinessFormOrderSku findOne($condition, $orderBy = [], $lockOption = '')
- * @method static Model_Orm_BusinessFormOrderSku[] findAll($cond, $orderBy = [], $offset = 0, $limit = null, $with = [])
- * @method static Generator|Model_Orm_BusinessFormOrderSku[] yieldAll($cond, $orderBy = [], $offset = 0, $limit = null)
- * @method static Model_Orm_BusinessFormOrderSku findOneFromRdview($condition, $orderBy = [], $lockOption = '')
+ * @method static Model_Orm_OrderSystemDetail findOne($condition, $orderBy = [], $lockOption = '')
+ * @method static Model_Orm_OrderSystemDetail[] findAll($cond, $orderBy = [], $offset = 0, $limit = null, $with = [])
+ * @method static Generator|Model_Orm_OrderSystemDetail[] yieldAll($cond, $orderBy = [], $offset = 0, $limit = null)
+ * @method static Model_Orm_OrderSystemDetail findOneFromRdview($condition, $orderBy = [], $lockOption = '')
  * @method static findRowFromRdview($columns, $condition, $orderBy = [])
- * @method static Model_Orm_BusinessFormOrderSku[] findAllFromRdview($cond, $orderBy = [], $offset = 0, $limit = null, $with = [])
+ * @method static Model_Orm_OrderSystemDetail[] findAllFromRdview($cond, $orderBy = [], $offset = 0, $limit = null, $with = [])
  * @method static findRowsFromRdview($columns, $cond, $orderBy = [], $offset = 0, $limit = null)
  * @method static findColumnFromRdview($column, $cond, $orderBy = [], $offset = 0, $limit = null)
  * @method static findValueFromRdview($column, $cond, $orderBy = [])
@@ -24,15 +25,15 @@
  * @method static findBySqlFromRdview($sql, $asArray = true)
  * @method static countFromRdview($cond, $column = '*')
  * @method static existsFromRdview($cond)
- * @method static Generator|Model_Orm_BusinessFormOrderSku[] yieldAllFromRdview($cond, $orderBy = [], $offset = 0, $limit = null)
+ * @method static Generator|Model_Orm_OrderSystemDetail[] yieldAllFromRdview($cond, $orderBy = [], $offset = 0, $limit = null)
  * @method static yieldRowsFromRdview($columns, $cond, $orderBy = [], $offset = 0, $limit = null)
  * @method static yieldColumnFromRdview($column, $cond, $orderBy = [], $offset = 0, $limit = null)
 */
 
-class Model_Orm_BusinessFormOrderSku extends Wm_Orm_ActiveRecord
+class Model_Orm_OrderSystemDetail extends Wm_Orm_ActiveRecord
 {
 
-    public static $tableName = 'business_form_order_sku';
+    public static $tableName = 'order_system_detail';
     public static $dbName = 'oms_order';
     public static $clusterName = 'oms_ordermis_cluster';
 }
--- models/orm/OrderSystemDetailSku.php
+++ models/orm/OrderSystemDetailSku.php
@@ -2,7 +2,7 @@
 
 /**
  * @property int $id
- * @property int $business_form_order_id
+ * @property int $order_id
  * @property int $sku_id
  * @property int $sku_amount
  * @property string $sku_ext
@@ -11,12 +11,12 @@
  * @property int $update_time
  * @property int $is_delete
  * @property int $version
- * @method static Model_Orm_BusinessFormOrderSku findOne($condition, $orderBy = [], $lockOption = '')
- * @method static Model_Orm_BusinessFormOrderSku[] findAll($cond, $orderBy = [], $offset = 0, $limit = null, $with = [])
- * @method static Generator|Model_Orm_BusinessFormOrderSku[] yieldAll($cond, $orderBy = [], $offset = 0, $limit = null)
- * @method static Model_Orm_BusinessFormOrderSku findOneFromRdview($condition, $orderBy = [], $lockOption = '')
+ * @method static Model_Orm_OrderSystemDetailSku findOne($condition, $orderBy = [], $lockOption = '')
+ * @method static Model_Orm_OrderSystemDetailSku[] findAll($cond, $orderBy = [], $offset = 0, $limit = null, $with = [])
+ * @method static Generator|Model_Orm_OrderSystemDetailSku[] yieldAll($cond, $orderBy = [], $offset = 0, $limit = null)
+ * @method static Model_Orm_OrderSystemDetailSku findOneFromRdview($condition, $orderBy = [], $lockOption = '')
  * @method static findRowFromRdview($columns, $condition, $orderBy = [])
- * @method static Model_Orm_BusinessFormOrderSku[] findAllFromRdview($cond, $orderBy = [], $offset = 0, $limit = null, $with = [])
+ * @method static Model_Orm_OrderSystemDetailSku[] findAllFromRdview($cond, $orderBy = [], $offset = 0, $limit = null, $with = [])
  * @method static findRowsFromRdview($columns, $cond, $orderBy = [], $offset = 0, $limit = null)
  * @method static findColumnFromRdview($column, $cond, $orderBy = [], $offset = 0, $limit = null)
  * @method static findValueFromRdview($column, $cond, $orderBy = [])
@@ -24,15 +24,15 @@
  * @method static findBySqlFromRdview($sql, $asArray = true)
  * @method static countFromRdview($cond, $column = '*')
  * @method static existsFromRdview($cond)
- * @method static Generator|Model_Orm_BusinessFormOrderSku[] yieldAllFromRdview($cond, $orderBy = [], $offset = 0, $limit = null)
+ * @method static Generator|Model_Orm_OrderSystemDetailSku[] yieldAllFromRdview($cond, $orderBy = [], $offset = 0, $limit = null)
  * @method static yieldRowsFromRdview($columns, $cond, $orderBy = [], $offset = 0, $limit = null)
  * @method static yieldColumnFromRdview($column, $cond, $orderBy = [], $offset = 0, $limit = null)
 */
 
-class Model_Orm_BusinessFormOrderSku extends Wm_Orm_ActiveRecord
+class Model_Orm_OrderSystemDetailSku extends Wm_Orm_ActiveRecord
 {
 
-    public static $tableName = 'business_form_order_sku';
+    public static $tableName = 'order_system_detail_sku';
     public static $dbName = 'oms_order';
     public static $clusterName = 'oms_ordermis_cluster';
 }
