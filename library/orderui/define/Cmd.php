9ae78e03b07a6e1f518fc519cff8cb0f45e70cb3
songhang <hang.song02@ele.me>
2018-04-08 21:29:57 +0800

    oms topic

--- library/orderui/define/Cmd.php
+++ library/orderui/define/Cmd.php
@@ -16,7 +16,7 @@ class Orderui_Define_Cmd
      * nwms topic
      * @var string
      */
-    const NWMS_ORDER_TOPIC = 'nwmsorder';
+    const OMS_ORDER_TOPIC = 'omsorderui';
 
     /**
      * nwms topic
@@ -24,68 +24,13 @@ class Orderui_Define_Cmd
      */
     const OMS_ENS_TOPIC = 'oms_ens';
 
-    /**
-     * nscm sync inbound
-     * @var string
-     */
-    const NSCM_SYNC_INBOUND = 'nscmsyncinbound';
 
-    /**
-     * 创建出库单命令
-     * @var string
-     */
-    const CMD_CREATE_STOCKOUT_ORDER = 'stockout_order_create';
-
-    /**
-     * 完成拣货命令
-     * @var string
-     */
-    const CMD_FINISH_PRICKUP_ORDER  = 'stockout_order_finish_pickup';
-
-    /**
-     * 作废出库单命令
-     * @var string
-     */
-    const CMD_DELETE_STOCKOUT_ORDER  = 'stockout_order_delete';
-
-
-    /**
-     * order statistics operate
-     * @var string
-     */
-    const CMD_SYNC_FORM_STATISTICS = 'order_statistics_operate';
-
-    /**
-     * reserve order create
-     * @var string
-     */
-    const CMD_CREATE_RESERVE_ORDER = 'reserve_order_create';
-
-    /**
-     * cmd sync inbound
-     * @var string
-     * @deprecated
-     */
-    const CMD_SYNC_INBOUND = 'sync_inbound';
-
-    /**
-     * cmd sync inbound nwms
-     * @var string
-     */
-    const CMD_SYNC_INBOUND_NWMS = 'nscm_purchase_order_sync';
-
-    /**
-<<<<<<< HEAD
+   /**
      * cmd signup stockout order
      * @var string
      */
     const CMD_SIGNUP_STOCKOUT_ORDER = 'signup_stockout_order';
 
-    /**
-     * cmd transmit signup data to tms
-     * @var string
-     */
-    const CMD_TRANSMIT_SIGNUP_DATA = 'transmit_signup_data_to_tms';
 
     /**
      * 创建销退入库单
@@ -104,7 +49,7 @@ class Orderui_Define_Cmd
      * @var array
      */
     const DEFAULT_WMQ_CONFIG = [
-        'Topic' => self::NWMS_ORDER_TOPIC,
+        'Topic' => self::OMS_ORDER_TOPIC,
         'Key' => '',
         'serviceName' => 'wmqproxy',
     ];
