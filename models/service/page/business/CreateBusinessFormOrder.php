d08b88d1e877e096a19abd95f93deac9becea8fd
jinyu02 <jinyu02@iwaimai.baidu.com>
2018-05-03 17:25:47 +0800

    split skus
    Change-Id: I3f4e27478ab6a87c39197a4d989b3899a323e34f

--- models/service/page/business/CreateBusinessFormOrder.php
+++ models/service/page/business/CreateBusinessFormOrder.php
@@ -27,10 +27,12 @@ class Service_Page_Business_CreateBusinessFormOrder
      * @throws Exception
      */
     public function execute($arrInput) {
+        $arrInput['business_form_order_way'] = Orderui_Define_BusinessFormOrder::ORDER_WAY_OBVERSE;
         if (Orderui_Define_BusinessFormOrder::BUSINESS_FORM_ORDER_TYPE_SHELF
             == $arrInput['business_form_order_type']) {
             return $this->objDsBusinessFormOrder->createOrder($arrInput);
         }
+        $arrInput['order_supply_type'] = Orderui_Define_BusinessFormOrder::ORDER_SUPPLY_TYPE_ORDER;
         Orderui_Wmq_Commit::sendWmqCmd(Orderui_Define_Cmd::CMD_CREATE_OMS_ORDER,
             $arrInput, $arrInput['logistics_order_id']);
         return [];
